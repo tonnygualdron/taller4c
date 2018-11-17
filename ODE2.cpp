7b59c7769f7dd3c60fe7716fcf72d67aa407f544
tonnygualdron <42391447+tonnygualdron@users.noreply.github.com>
2018-11-16 20:51:32 -0500

    version 3
    metodo runge kutta 4 funciona, solo falta segunda parte

--- ODE2.cpp
+++ ODE2.cpp
@@ -8,7 +8,7 @@ using namespace std;
 float norma(float x, float y);
 float ax(float c, float m,float vx, float vy, float g);
 float ay(float c, float m,float vx, float vy, float g);
-void rk4(float c, float m,float vx, float vy, float g, float x,float y,float h, float t, FILE *d);
+float rk4(float c, float m,float vx, float vy, float g, float x,float y,float h, float t, FILE *d);
 void imprimir(float t,float x, float y,float vx, float vy, FILE *d);
 
 int main()
@@ -57,9 +57,11 @@ int main()
  FILE *output;
  output=fopen("45grados.txt", "w");
  
- rk4(c,m,vx, vy,g,x,y,dt, t, output);
+ float xf = rk4(c,m,vx, vy,g,x,y,dt, t, output);
+ cout << "La distancia recorrida fue " << xf << " m." << endl;
  
- fclose(output);
+fclose(output);
+
  
  return 0;
 }
@@ -88,9 +90,10 @@ void imprimir(float t,float x, float y,float vx, float vy, FILE *d){
  fprintf(d, "%f %f %f %f %f\n", t,x,y,vx,vy);
 }
 
-void rk4(float c, float m,float vx, float vy, float g, float x,float y,float h, float t, FILE *d){
+float rk4(float c, float m,float vx, float vy, float g, float x,float y,float h, float t, FILE *d){
  bool indicador = true;
  int i = 0;
+ imprimir(t,x,y,vx, vy, d);
  while(indicador){
   float k1x=0.0;
   float k2x=0.0;
@@ -101,30 +104,53 @@ void rk4(float c, float m,float vx, float vy, float g, float x,float y,float h, 
   float k2y=0.0;
   float k3y=0.0;
   float k4y=0.0;
+  
+  float k1xi=0.0;
+  float k2xi=0.0;
+  float k3xi=0.0;
+  float k4xi=0.0;
+
+  float k1yi=0.0;
+  float k2yi=0.0;
+  float k3yi=0.0;
+  float k4yi=0.0;
 
   float xActual=0.0;
   float yActual=0.0;
   float vxActual=0.0;
   float vyActual=0.0;
 
-  k1x = h*ax(c,m,vx,vy,g);
-  k1y = h*ay(c,m,vx,vy,g);
+  k1x = ax(c,m,vx,vy,g);
+  k1y = ay(c,m,vx,vy,g);
 
-  k2x = h*ax(c,m,vx+0.5*k1x,vy+0.5*k1y,g);
-  k2y = h*ay(c,m,vx+0.5*k1x,vy+0.5*k1y,g);
+  k2x = ax(c,m,vx+0.5*h*k1x,vy+0.5*h*k1y,g);
+  k2y = ay(c,m,vx+0.5*h*k1x,vy+0.5*h*k1y,g);
 
-  k3x = h*ax(c,m,vx+0.5*k2x,vy+0.5*k2y,g);
-  k3y = h*ay(c,m,vx+0.5*k2x,vy+0.5*k2y,g);
+  k3x = ax(c,m,vx+0.5*h*k2x,vy+0.5*h*k2y,g);
+  k3y = ay(c,m,vx+0.5*h*k2x,vy+0.5*h*k2y,g);
 
-  k4x = h*ax(c,m,vx+k3x,vy+k3y,g);
-  k4y = h*ay(c,m,vx+k3x,vy+k3y,g);
+  k4x = ax(c,m,vx+h*k3x,vy+h*k3y,g);
+  k4y = ay(c,m,vx+h*k3x,vy+h*k3y,g);
   
 
-  vxActual= vx + ((1.0/6.0)*(k1x+2*k2x+2*k3x+k4x));
-  vyActual= vy + ((1.0/6.0)*(k1y+2*k2y+2*k3y+k4y));
+  vxActual= vx + ((1.0/6.0)*(k1x+2*k2x+2*k3x+k4x)*h);
+  vyActual= vy + ((1.0/6.0)*(k1y+2*k2y+2*k3y+k4y)*h);
+
   
-  xActual= x + vxActual*h;
-  yActual= y + vyActual*h;
+  k1xi = vx;
+  k1yi = vy;
+
+  k2xi = vx + k1xi*h*0.5;
+  k2yi = vy + k1yi*h*0.5;
+
+  k3xi = vx + k2xi*h*0.5;
+  k3yi = vy + k2yi*h*0.5;
+
+  k4xi = vx + k3xi*h;
+  k4yi = vy + k3yi*h;
+  
+  xActual= x + ((1.0/6.0)*(k1xi+2*k2xi+2*k3xi+k4xi)*h);
+  yActual= y + ((1.0/6.0)*(k1yi+2*k2yi+2*k3yi+k4yi)*h);
 
   x=xActual;
   y=yActual;
@@ -138,4 +164,5 @@ void rk4(float c, float m,float vx, float vy, float g, float x,float y,float h, 
    indicador = false;
   }
  }
+ return x;
 }
